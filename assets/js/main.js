53f003e2d6a0463a3103be85355940cc921856c2
Colin Rubbert <colinrubbert@gmail.com>
2017-01-26 15:52:11 -0600

    Reconceptualize site, build out nav & about

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,15 @@
 // Main JavaScript File
+
+var toggleButton = document.getElementById("navigation-menu-toggle").classList;
+var menu = document.getElementById("navigation-menu-wrapper").classList;
+
+function toggleMenu() {
+  menu.toggle("open");
+  if (toggleButton.contains("closed")) {
+    toggleButton.remove("closed");
+    toggleButton.add("opened");
+  } else {
+    toggleButton.remove("opened");
+    toggleButton.add("closed");
+  }
+}
